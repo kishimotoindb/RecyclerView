a0265b761507855eb2abb15783c06e918990b507
fearlessBear <519376470@qq.com>
2017-03-28 11:17:04 +0800

    item decoration

--- app/src/main/java/com/example/root/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/root/recyclerview/MainActivity.java
@@ -2,6 +2,17 @@ package com.example.root.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/*
+ *
+
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recycler = (RecyclerView) findViewById(R.id.recycler);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recycler.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return new Holder(View.inflate(MainActivity.this, R.layout.item, null));
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            }
+
+            @Override
+            public int getItemCount() {
+                return 2;
+            }
+        });
+
+    }
+
+    static class Holder extends RecyclerView.ViewHolder {
+        TextView mTextView;
+
+        public Holder(View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView.findViewById(R.id.tv);
+        }
     }
 }
